903bde956c87eb7e5b564ce9e84f0bcaee2d9c7d
sbalta01 <saulbaltasar08@gmail.com>
2025-05-20 14:03:09 +0200

    Updated demo with new alpaca api

--- alpaca_demo.py
+++ alpaca_demo.py
@@ -1,8 +1,12 @@
+import os
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
 from alpaca.trading.client import TradingClient
-from alpaca.trading.enums import Orderside, TimeInForce
 from alpaca.trading.requests import MarketOrderRequest
-from alpaca.trading.stream import TradingStream
-
+from alpaca.trading.enums import OrderSide, TimeInForce
+from datetime import datetime, timedelta
+import pandas as pd
 
 # ── CONFIG ─────────────────────────────────────────────────────────
 from dotenv import load_dotenv
@@ -15,7 +19,13 @@ API_KEY    = os.getenv("APCA_API_KEY_ID")
 API_SECRET = os.getenv("APCA_API_SECRET_KEY")
 BASE_URL   = "https://paper-api.alpaca.markets"
 
-client = TradingClient(API_KEY, API_SECRET, paper = True)
-account = dict(client.get_account())
+PAPER = True  # Set to False for live trading
+
+# Clients
+trading_client = TradingClient(API_KEY, API_SECRET, paper=PAPER)
+data_client = StockHistoricalDataClient(API_KEY, API_SECRET)
+
+account = dict(trading_client.get_account())
+
 for k,v in account.items():
     print(f"{k:30}{v}")
