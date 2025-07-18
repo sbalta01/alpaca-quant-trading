c08f7bbc5f326fcf39ec2dcf8437273e62214c95
sbalta01 <saulbaltasar08@gmail.com>
2025-07-19 00:17:00 +0200

    Portfolio optimization implemented

--- main/run_monte_carlo.py
+++ main/run_monte_carlo.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 import numpy as np
 from src.data.data_loader import fetch_nasdaq_100_symbols
-from src.utils.monte_carlo import monte_carlo_portfolio_risk, plot_monte_carlo_results
+from src.utils.monte_carlo import monte_carlo_portfolio_risk
 
 # symbols = ["SPY"]
 # symbols = ["AAPL","AMZN","MSFT","GOOG", "VRTX"]
@@ -19,10 +19,10 @@ from src.utils.monte_carlo import monte_carlo_portfolio_risk, plot_monte_carlo_r
 
 symbols = fetch_nasdaq_100_symbols()
 
-start   = datetime(2023, 7, 1)
-# start   = datetime(2025, 1, 5)
 end     = datetime.now()
-# end     = datetime(2025, 1, 1)
+# end     = datetime(2023, 1, 1)
+start = end - timedelta(days=2*365)
+# start   = datetime(2024, 1, 1)
 timeframe = TimeFrame.Day  # or pd.Timedelta(days=1)
 feed = 'iex'
 
@@ -67,18 +67,38 @@ except:
         )
     
 
-results = monte_carlo_portfolio_risk( #10-day horizon over >250 days of data is good
-    price_hist   = df,                    # multiindex or wide
-    # T            = 11/252, #In years. n_steps = T - 1
-    T            = 1, #In years. n_steps = T - 1
-    dt           = 1/252, #1/252 = 1 day
-    n_sims       = 10000,
-    alpha_empirical= 0.05,
-    alpha_evt    = 0.01,
-    trials       = 50, #Number of models to run to optimize position sizing
-    random_state = 43,
-    backtest = False, #Backtesting uses all data up until -T to compute GBM parameters and starts predicting from -T. It is equivalent to predicting from -T.
-)
-
 # VaR: With (95%) confidence, my returns are above VaR.
 # CVaR: If the loss does exceed VaR (returns below VaR), the average loss is CVaR.
+
+
+monte_carlo_simulator = monte_carlo_portfolio_risk( #10-day horizon over >250 days of data is good
+        price_hist   = df,                    # multiindex or wide
+        T            = 26/252, #In years. n_steps = T - 1
+        # T            = 1, #In years. n_steps = T - 1
+        dt           = 1/252, #1/252 = 1 day
+        n_sims       = 10_000,
+        alpha_empirical= 0.05,
+        alpha_evt    = 0.01,
+        returns_penalty = 1.0,
+        volat_penalty = 1.0,
+        cvar_penalty = 1.0,
+        random_state = 43,
+        predict = False, #Backtesting uses all data up until -T to compute GBM parameters and starts predicting from -T. It is equivalent to predicting from -T.
+        optimize_position_sizing = True,
+    )
+
+
+best_port_paths, optimized_portfolio, empirical, evt = monte_carlo_simulator.simulate_portfolio()
+
+
+print("Portfolio:")
+print(optimized_portfolio.sort_values(by="position", axis = 0, ascending=False).loc[optimized_portfolio["position"]>1e-3])
+print(f"\nPortfolio VaR (95%): {empirical[0]:.2%}")
+print(f"Portfolio CVaR (5% tail): {empirical[1]:.2%}")
+
+
+monte_carlo_simulator.plot_monte_carlo_results(best_port_paths,
+                        empirical,
+                        evt,
+                        optimized_portfolio["position"],
+                        )
